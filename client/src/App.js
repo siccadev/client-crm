1068aa8e30190080d877b86db2b6081b3f873480
IyeddAmri <iyedamri21@gmail.com>
2024-06-15 12:46:37 +0100

    f

--- client/src/App.js
+++ client/src/App.js
@@ -10,15 +10,17 @@ import Home from './home/Home';
 import Dashboard from './home/dashboard';
 import GestionOpportunites from './dashboardadmin/GestionOpportunites';
 import Profile from './dashboardclient/profile.js';
-import Total from './dashboardclient/Total.js'
-import Approved from './dashboardclient/Approved.js'
+import Total from './dashboardclient/Total.js';
+import Approved from './dashboardclient/Approved.js';
 import Notapproved from './dashboardclient/Notapproved.js';
-import Processing from './dashboardclient/Processing.js'
-import Contract from './dashboardclient/Contarct.js'
+import Processing from './dashboardclient/Processing.js';
+import Contract from './dashboardclient/Contarct.js';
 import SuiviPaiement from './dashboardadmin/suivipaiment.js'; // Import SuiviPaiement component
+// Import PaymentStatistics component
+import Statistiques from './dashboardadmin/Statistiques.js'; // Import Statistiques component
 import { RecoilRoot } from 'recoil';
-import risk from "./dashboardadmin/GestionRisques.js"
 
+import risk from "./dashboardadmin/GestionRisques.js";
 
 function App() {
   return (
@@ -43,7 +45,8 @@ function App() {
           <Route path="/" element={<DashboardClient />} />
           <Route path="/Contract" element={<Contract />} />
           <Route path="/late-payments" element={<SuiviPaiement />} /> {/* Add this line for SuiviPaiement component */}
-
+          <Route path="/payment-statistics" element={<Statistiques />} /> {/* Add this line for PaymentStatistics component */}
+          <Route path="/statistiques" element={<Statistiques />} /> {/* Add this line for Statistiques component */}
           <Route path="/gestion-opportunites/:status" element={<GestionOpportunites />} />
         </Routes>
       </Router>
